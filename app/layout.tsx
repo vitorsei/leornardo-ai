ac1c1d1b3851a1a75995d8433f5d32cbad4e3cca
vitor.iwamura <vitor.iwamura@bytedance.com>
2025-05-14 00:24:01 +1000

    Initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Provider } from "@/components/ui/provider";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Rick and Morty Explorer",
+  description: "Rick and Morty Explorer - Leonardo AI",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Provider>{children}</Provider>
+      </body>
+    </html>
+  );
+}
